b2288d212e7e98c64b4fbda517f486736935efbf
Vladislav <vlad_kadun@vk.com>
2025-06-30 02:41:54 +0700

    update syntax.h

--- syntax.h
+++ syntax.h
@@ -1,6 +1,22 @@
 #ifndef SYNTAX_H
 #define SYNTAX_H
 
+typedef enum {
+    DIGIT,
+    PREFIX_FUNCTION,
+    LBRACKET,
+    RBRACKET,
+    BINAR_OPERATION
+} Syntax;
+
+void push(Lexem lexem, Lexem** array, size_t* size);
+
+Lexem pop(Lexem** array, size_t* size);
+
+int get_syntax(Lexem lexem);
+
+int get_priority(Lexem lexem);
+
 Lexem* to_RPN(Lexem* lexems);
 
 #endif
